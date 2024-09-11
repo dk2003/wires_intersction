6894fb30c33daf61ea3312213db635b4e2c49f02
root <1813560139@qq.com>
2024-09-11 22:02:17 +0800

    second

--- Mass_detect/index.py
+++ Mass_detect/index.py
@@ -1,13 +1,35 @@
 import cv2
+import os
 import numpy as np
 
+# 细化，前景为白，背景为黑
+def thin(url):
+    # 处理路径
+    full_image_path = os.path.join(os.getcwd(), url)
+
+    # 读取图像
+    image = cv2.imread(full_image_path)
+
+    # 转化为灰度图
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # 二值化
+    # _, binary_image = cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY)
+    _, binary_image = cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY_INV)
+
+    # 细化，要求前景是白色，背景是黑色
+    thinned_image = cv2.ximgproc.thinning(binary_image)
+
+    return thinned_image
+
+# 获取 8 邻域像素
 def neighbours_fn(x,y,image):
     """Return 8-neighbours of image point P1(x,y), in a clockwise order"""
     img = image
     x_1, y_1, x1, y1 = x-1, y-1, x+1, y+1
     return [ img[x_1][y], img[x_1][y1], img[x][y1], img[x1][y1], img[x1][y], img[x1][y_1], img[x][y_1], img[x_1][y_1] ]   
 
-
+# 获取交点
 def getSkeletonIntersection(skeleton):
     validIntersection = [[0,1,0,1,0,0,1,0],[0,0,1,0,1,0,0,1],[1,0,0,1,0,1,0,0],
                          [0,1,0,0,1,0,1,0],[0,0,1,0,0,1,0,1],[1,0,0,1,0,0,1,0],
@@ -43,23 +65,18 @@ def getSkeletonIntersection(skeleton):
     intersections = list(set(intersections))
     return intersections
 
+# 绘制交点并且保存
+def intersections_detect(inputUrl,outputUrl):
+    skeleton = thin(inputUrl)
+    inverted_image = 255 - skeleton
+    # 前景边黑、背景变白
+    intersection_points = getSkeletonIntersection(skeleton)
+    # 在图像上绘制交点
+    for point in intersection_points:
+        cv2.circle(inverted_image, (int(point[0]), int(point[1])), 10, (0, 255, 0), -1)
+    cv2.imwrite(outputUrl, inverted_image)
 
-url = './images/demo1_mask3.jpg'
-# 读取彩色图像
-image_color = cv2.imread(url, cv2.IMREAD_COLOR)  # 或者使用 1 代替 cv2.IMREAD_COLOR
+if __name__=='__main__':
+    intersections_detect('./input/mask.jpg','./output/intersection.jpg')
 
-# 转换成灰度图像
-image = cv2.cvtColor(image_color, cv2.COLOR_BGR2GRAY)
-
-# 将图像转换为二值图像
-_, image1 = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
-
-intersection_points = getSkeletonIntersection(image1)
-
-
-# 在图像上绘制交点
-for point in intersection_points:
-    cv2.circle(image_color, (int(point[0]), int(point[1])), 10, (0, 255, 0), -1)
-
-output_filename = './output/res3.jpg'
-cv2.imwrite(output_filename, image_color)
+__all__ = ['intersections_detect']
